08a5c6a2f89c0372a63351de817f100f3a24a01f
Bruno Fernandes Valero <brunofvn6@gmail.com>
2025-08-20 23:36:21 -0300

    add: ft_issign, update: ft_destroy_char_matrix

--- src/libstr/src/ft_destroy_char_matrix.c
+++ src/libstr/src/ft_destroy_char_matrix.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   destroy_char_matrix.c                               :+:      :+:    :+:   */
+/*   ft_destroy_char_matrix.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: valero <valero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/20 22:46:47 by valero            #+#    #+#             */
-/*   Updated: 2025/08/20 22:58:54 by valero           ###   ########.fr       */
+/*   Created: 2025/08/20 23:33:27 by valero            #+#    #+#             */
+/*   Updated: 2025/08/20 23:33:33 by valero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libstr.h"
 
-void	*destroy_char_matrix(char ***char_matrix_ref)
+void	*ft_destroy_char_matrix(char ***char_matrix_ref)
 {
 	int	i;
 
